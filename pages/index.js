4d682df8d190c00b583e83d0320fb1afb5b30ed7
Jamie Wade <jamiewade94@icloud.com>
2021-10-24 18:30:47 +0100

    Basic working GraphQL instance with Apollo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from "next/head"
-import Image from "next/image"
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-export default function Home() {
+export default function Home({ blogPosts }) {
+	console.log('blogPosts', blogPosts);
+
 	return (
 		<div>
 			<h1>Welcome</h1>
@@ -9,4 +10,27 @@ export default function Home() {
 			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam aliquid nemo dicta mollitia ab explicabo temporibus nisi illum molestias unde odit magni soluta aut nulla iusto, modi minus quaerat atque.</p>
 		</div>
 	)
+}
+
+export async function getStaticProps() {
+	const client = new ApolloClient({
+		uri: 'http://cms.test/api/',
+		cache: new InMemoryCache()
+	});
+
+	const { data } = await client.query({
+		query: gql`
+			query GetBlogPosts {
+				entries (section: "blog") {
+					title
+				}
+			}
+		`
+	});
+
+	return {
+		props: {
+			blogPosts: data.entries
+		}
+	}
 }
