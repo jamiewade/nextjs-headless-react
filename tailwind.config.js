2c4850e5920c07eb759cc9162585de4d0901cf2f
Jamie Wade <jamiewade94@icloud.com>
2021-10-25 16:41:57 +0100

    Google Fonts implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	darkMode: false,
 	theme: {
 		container: {
 			center: true
+		},
+		fontFamily: {
+			sans: ["Roboto", "sans-serif"]
 		},
 		extend: {},
 	},
